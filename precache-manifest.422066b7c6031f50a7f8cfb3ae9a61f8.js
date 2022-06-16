9753bcf2f483a9013624a561017d9c60ed1d6871
Cristian Calderon <cristianfab90@gmail.com>
2022-06-16 14:41:34 +0200

    Updates

---
+++ precache-manifest.422066b7c6031f50a7f8cfb3ae9a61f8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0da4738b7f827f596d6",
+    "url": "/Portfolio/static/css/main.16a767dd.chunk.css"
+  },
+  {
+    "revision": "d0da4738b7f827f596d6",
+    "url": "/Portfolio/static/js/main.d0da4738.chunk.js"
+  },
+  {
+    "revision": "ac167ac524470cab3665",
+    "url": "/Portfolio/static/js/1.ac167ac5.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/Portfolio/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "4b4008e69fb210e46ef44523cd672e65",
+    "url": "/Portfolio/static/media/cv.4b4008e6.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/Portfolio/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "c11a9ce8cef692ec871e3e31f28e3c8f",
+    "url": "/Portfolio/static/media/Pokered.c11a9ce8.png"
+  },
+  {
+    "revision": "20b112f50bda6516bfb41c73d651896b",
+    "url": "/Portfolio/static/media/Re-educ.20b112f5.png"
+  },
+  {
+    "revision": "08a75b496b53a43a46b62e4b5da84941",
+    "url": "/Portfolio/static/media/Tenet.08a75b49.png"
+  },
+  {
+    "revision": "3d36c210cb16211038aba3b3ff059ad2",
+    "url": "/Portfolio/static/media/bg-texture.3d36c210.png"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Portfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Portfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Portfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Portfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Portfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Portfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "b1583d0efc11cf0486238dfc8efedebb",
+    "url": "/Portfolio/index.html"
+  }
+];
