270c2967d1b87b352d0e39107236b31e60c12f71
Cristian Calderon <cristianfab90@gmail.com>
2022-06-29 18:47:07 +0200

    fixed links footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,11 @@ const Footer = () => {
             {/* <a href="/#" className="footer_logo"> EGATOR</a> */}
 
             <ul className="permalinks">
-                <li><a href="/#">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#experience">Experience</a></li>
-                <li><a href="/#portfolio">Portfolio</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
             <div className="footer_socials">
                 <a href="https://twitter.com/cris_fab" target='-blank'><BsTwitter /></a>
